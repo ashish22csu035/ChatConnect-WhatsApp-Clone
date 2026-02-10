a4a3b7e9ec0b7522118bb8da3f473950e2af0e58
ashish22csu035 <141615734+ashish22csu035@users.noreply.github.com>
2026-02-11 00:29:33 +0530

    Fix: Express route handler and disable passport sessions6

--- server/server.js
+++ server/server.js
@@ -36,6 +36,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.set('trust proxy', 1);
 
 // Session setup for Passport
 app.use(session({
